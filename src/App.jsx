ea3ee22f8263563d7ffd94a1eda4f90bc8a1c6a6
Sunny-2610 <sunnysinha9884@gmail.com>
2025-07-20 12:07:12 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import Header from './Components/Header'
+import About from './Components/About'
+import Projects from './Components/Projects'
 
 const App = () => {
   return (
-    <div>
+    <div className='w-full overflow-hidden'>
      
       <Header />
+      <About />
+      <Projects />
     </div>
   )
 }
