e550f0cd376bfae7b57de507d7fac05480bba8b0
Sunny-2610 <sunnysinha9884@gmail.com>
2025-07-20 18:01:01 +0530

    add contactus sectio

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Header from './Components/Header'
 import About from './Components/About'
 import Projects from './Components/Projects'
 import Testimonails from './Components/Testimonails'
-
+import Contact from './Components/Contact'
+ import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+       <ToastContainer />
      
       <Header />
       <About />
       <Projects />
       <Testimonails />
+      <Contact />
     </div>
   )
 }
